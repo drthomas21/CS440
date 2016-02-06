9ba850c0f37f04f99ba41f09197f780d83f6982b
David Thomas <david.thomas214@yahoo.com>
2016-02-06 14:02:53 -0800

    maybe this will work

--- Homework_2/program002.cpp
+++ Homework_2/program002.cpp
@@ -3,17 +3,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h> 
 #include <sys/mman.h>
 
 const double AMOUNT = 10.00;
-static double *account;
+
+class Account {
+	private:
+		double savings = 0;
+		double checking = 0;
+public:
+	Account(): savings(0), checking(0) { };
+	Account(double savings, double checking) :savings(savings), checking(checking) { }
+	bool transferFunds(double amount) {
+		if (checking >= amount) {
+			checking -= amount;
+			savings += amount;
+			return true;
+		}
+
+		return false;
+	}
+	void showAccounts() {
+		std::cout << "\n\tSavings: $" << savings
+			<< "\n\tChecking: $" << checking
+			<< std::endl;
+	}
+};
 
 int main() {
+	srand(static_cast<int>(time(NULL)));
 	int clients = 0;
-	account = static_cast<double*>(mmap(NULL, sizeof *account, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
-	*account = 0;
 	
 	while (clients == 0) {
 		std::cout << "How many clients? " << std::endl;
@@ -25,6 +47,23 @@ int main() {
 			std::cin.ignore(std::numeric_limits<int>::max(), '\n');
 		}
 	}
+
+	Account account;
+	Account *pAccounts = &account;
+	
+
+	pAccounts = static_cast<Account*>(mmap(NULL, (sizeof *pAccounts) * clients, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+	Account *_pAccounts = pAccounts;
+	for (int i = 0; i < clients; i++) {
+		pAccount = _pAccounts + sizeOf(account) * i;
+		*pAccounts = new Account(rand() % 100, rand() % 100 + 10);
+		std::cout << "Client " << (i + 1) << ": ";
+		accounts[i]->showAccounts();
+	}
+
+	std::cout << "[Press 'ENTER' to continue]" << std::endl << std::endl;
+	std::cin.ignore();
+
 	pid_t *pid = new pid_t[clients];
 	for (int i = 0; i < clients; i++) {
 		pid[i] = fork();
@@ -33,7 +72,8 @@ int main() {
 			std::cout << "Epic Error In Forking" << std::endl;
 			return 1;
 		} else if (pid[i] == 0) {
-			*account += AMOUNT;
+			pAccount = _pAccounts + sizeOf(account) * i;
+			(*pAccounts)->transferFunds(AMOUNT);
 			return 0;
 		}
 	}
@@ -42,8 +82,16 @@ int main() {
 		int status;
 		while (waitpid(pid[i], &status, 0) == -1);
 	}
-	std::cout << "Savings Account Balance: $" << *account << std::endl;
-	munmap(account, sizeof *account);
+	
+	for (int i = 0; i < clients; i++) {
+		pAccount = _pAccounts + sizeOf(account) * i;
+		*pAccounts = new Account(rand() % 100, rand() % 100 + 10);
+		std::cout << "Client " << (i + 1) << ": ";
+		accounts[i]->showAccounts();
+	}
+
+	munmap(pAccounts, sizeof *account);
 	delete pid;
+	delete _pAccounts;
 	return 0;
 }
